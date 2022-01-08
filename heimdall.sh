995b1e294c4e1e83932be01e968e7d0fb7b9a676
Jonny <royalpython2@hotmail.com>
2022-01-08 07:26:45 +0000

    Example heimdall bash script for update_upgrade.yml playbook

--- heimdall.sh
+++ heimdall.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-oldsha=`cat /home/jonny/heimsha.txt`
+# on first launch the sha file won't exist, so will recreate a container regardless.
+# once the script has run once, sha file will be created and script will ignore
+# updates if the image exists on system. Personalize shafile variable/directory
+# to meet your needs.
+
+shafile='/home/user/heimsha.txt'
+
+oldsha=`cat $shafile`
 newsha=$(docker pull ghcr.io/linuxserver/heimdall | grep sha256 | awk '{print $2}' | awk -F ':' '{print $2}')
-echo $oldsha old
-echo $newsha new
 
 if [[ $oldsha == $newsha ]]
 then
 	echo 'Image used is  already latest version'
 	exit 0
 else
-	docker pull ghcr.io/linuxserver/heimdall | grep sha256 | awk '{print $2}' | awk -F ':' '{print $2}' > /home/jonny/heimsha.txt
+	docker pull ghcr.io/linuxserver/heimdall | grep sha256 | awk '{print $2}' | awk -F ':' '{print $2}' > $shafile
 
 	answer=$(sudo docker ps -a | grep heimdall | awk '{print $1}')
 	echo "Container $answer will be removed, and a new container will be created"
